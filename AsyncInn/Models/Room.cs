0a9b6646c0aa360b2843f97fc52efc571a0a4240
Jon Rice <revyolution1120@gmail.com>
2019-06-27 09:04:12 -0700

    Added models and basic setup of models.

---
+++ AsyncInn/Models/Room.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AsyncInn.Models
+{
+    public class Room
+    {
+        public string Name { get; set; }
+        public string Layout { get; set; }
+    }
+}
