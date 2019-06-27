8431cc08aa882ab9ee7d1ff622383983ed976c8f
Jon Rice <revyolution1120@gmail.com>
2019-06-26 19:44:54 -0700

    Setting up app.

--- AsyncInn/Startup.cs
+++ AsyncInn/Startup.cs
@@ -15,7 +15,7 @@ namespace AsyncInn
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMVC();
+            services.AddMvc();
 
         }
 
