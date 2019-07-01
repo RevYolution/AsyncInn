fdef35f4e1a5faf80a978d48b3e9a3fd22f4d672
Jon Rice <revyolution1120@gmail.com>
2019-07-01 10:00:57 -0700

    interfaces for hotel rooms and amenities complete.

---
+++ AsyncInn/Models/Interfaces/IAmenity.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AsyncInn.Models.Interfaces
+{
+    interface IAmenity
+    {
+        /// <summary>
+        /// Add an amenity to a room
+        /// </summary>
+        /// <param name="amenities"></param>
+        /// <returns></returns>
+        Task AddAmenity(Amenities amenities);
+
+        /// <summary>
+        /// Update an amenity for a room
+        /// </summary>
+        /// <param name="amenities"></param>
+        /// <returns></returns>
+        Task UpdateAmenity(Amenities amenities);
+
+        /// <summary>
+        /// Deletes an amenity from a room 
+        /// </summary>
+        /// <param name="amenities"></param>
+        /// <returns></returns>
+        Task DeleteAmenity(Amenities amenities);
+
+        /// <summary>
+        /// Provides all the amenities
+        /// </summary>
+        /// <returns></returns>
+        Task<List<Amenities>> GetAmenities();
+
+        /// <summary>
+        /// Gets a single amenity 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        Task<Amenities> GetAmenities(int? id);
+    }
+}
---
+++ AsyncInn/Models/Interfaces/IHotel.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AsyncInn.Models.Interfaces
+{
+    interface IHotel
+    {
+        /// <summary>
+        /// Add a hotel 
+        /// </summary>
+        /// <param name="hotel"></param>
+        /// <returns></returns>
+        Task AddHotel(Hotel hotel);
+
+        /// <summary>
+        /// Delete a hotel
+        /// </summary>
+        /// <param name="hotel"></param>
+        /// <returns></returns>
+        Task DeleteHotel(Hotel hotel);
+
+        /// <summary>
+        /// Update hotel
+        /// </summary>
+        /// <param name="hotel"></param>
+        /// <returns></returns>
+        Task UpdateHotel(Hotel hotel);
+
+        /// <summary>
+        /// Gets a list of all hotels 
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        Task<List<Hotel>> GetHotels(string name = null);
+
+        /// <summary>
+        /// Gets an individual hotel
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        Task<Hotel> GetHotel(int? id);
+
+        /// <summary>
+        /// Checks if a hotel room exists. 
+        /// </summary>
+        /// <param name="id">Hotel room id</param>
+        /// <param name="roomNumber">Room number to check</param>
+        /// <returns>True/False if room present</returns>
+        Task<bool> HotelRoomIsPresent(int id, int roomNumber);
+
+        /// <summary>
+        /// Adds a hotel room to a hotel
+        /// </summary>
+        /// <param name="hotelRoom"></param>
+        /// <returns></returns>
+        Task AddHotelRoom(HotelRoom hotelRoom);
+
+        /// <summary>
+        /// Deletes a hotel room from a hotel 
+        /// </summary>
+        /// <param name="hotelRoom"></param>
+        /// <returns></returns>
+        Task DeleteHotelRoom(HotelRoom hotelRoom);
+
+        /// <summary>
+        /// Updates a hotel room in a hotel. 
+        /// </summary>
+        /// <param name="hotelRoom"></param>
+        /// <returns></returns>
+        Task UpdateHotelRoom(HotelRoom hotelRoom);
+
+        /// <summary>
+        /// Gets hotel rooms 
+        /// </summary>
+        /// <returns></returns>
+        Task<List<HotelRoom>> GetHotelRooms();
+
+        /// <summary>
+        /// Gets a single room. 
+        /// </summary>
+        /// <param name="hotelID"></param>
+        /// <param name="roomNumber"></param>
+        /// <returns></returns>
+        Task<HotelRoom> GetHotelRoom(int hotelID, int roomNumber);
+
+        
+    }
+}
